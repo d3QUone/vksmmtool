e0a657aca5ee2932acc6946bddb9b115c59daaf0
Vladimir <volkvid@yandex.ru>
2015-01-19 02:07:44 +0300

    fixes
    new method to access closed communities + some fixes

--- vkparser.py
+++ vkparser.py
@@ -23,21 +23,27 @@ def save_log(text):
 
 # request interface - OK
 def getA(group_id, auth_token=None, offset=0, count=100):
-    url = "https://api.vk.com/method/wall.get"
-    data = { "owner_id": "-{0}".format(group_id),
-             "offset": offset,
-             "count": count,
-             "v": "5.27"
-             }
     if auth_token:
-        data["access_token"] = auth_token
+        url = "https://api.vk.com/method/execute.wall"
+        data = {"i": "-{0}".format(group_id),
+                "o": offset,
+                "c": count,
+                "access_token": auth_token
+            }
+    else:
+        url = "https://api.vk.com/method/wall.get"
+        data = { "owner_id": "-{0}".format(group_id),
+                 "offset": offset,
+                 "count": count,
+                 "v": "5.27"
+                 }
     res = requests.get(url, params = data)
     try:
         rjson = res.json()
         if "response" in rjson.keys():
             return rjson["response"]
         else:
-            #print "fuck: {0}".format(rjson["error"])
+            print "\nfuck: {0}".format(rjson["error"])
             return {"code": rjson["error"]["error_code"], "message": rjson["error"]["error_msg"]}
     except Exception as e:
         save_log("Requests error: {0}".format(e))
@@ -84,16 +90,20 @@ def controller():
     append = processed_groups.append
     while True:
         t = datetime.now()
-        all_groups = get_unique_groups("select added, group_id from groups")
+                                                                             #GAP HERE!!!
+        all_groups = get_unique_groups("select added, group_id from groups") #where user_id = 51758590")
         try:
             new_id = full_cycle_v2(processed_groups, all_groups)
-            print "new_id", new_id, "saved"
             append(new_id)
+            print "new_id={0} saved".format(new_id)
         except Exception as e:
             save_log("full_cycle error: {0}".format(e))
             print "full_cycle error: {0}".format(e)
+            print "\nlen(processed_groups)={0}, len(all_groups)={1}".format(len(processed_groups), len(all_groups))
+            time.sleep(3)
+            
         if len(all_groups) == len(processed_groups):
-            print "all groups were updated!".upper()
+            print "\nall groups were updated!".upper()
             del processed_groups[:]
             
         # update stats
@@ -113,10 +123,8 @@ def controller():
         stat = open(os.getcwd() + '/statistics.txt', 'w')
         stat.write(json.dumps({"totalgroups": "{0}".format(len(all_groups)), "totalposts": 0, "time": dt, "last_update": last_update}))
         stat.close()
-
         print "--"*25
-        print "new cycle:\n"
-    
+
 
 # actually scans only 1 group, save parsed groups 
 def full_cycle_v2(processed_groups, all_):
@@ -136,37 +144,40 @@ def full_cycle_v2(processed_groups, all_):
                     i += 1
             else: # this group is already parsed
                 all_groups.pop(i)
-                
         except Exception as e:
             print "Allert: data error, {0}. Raw: {1}".format(e, item)
-
-    # process chosen id           
-    if chosen_id != -1:
-        group_id = chosen_id
-    else:
-        group_id = all_groups[0][1] # -- first group of leftover
+    try:
+        # process chosen id           
+        if chosen_id != -1:
+            group_id = chosen_id
+        else:
+            group_id = all_groups[0][1] # -- first group of leftover
+    except Exception as e:
+        print "choose_id exception: {0}".format(e)
+    
     auth_token = None
     ret = getA(group_id, auth_token, 0, 1)
-
+    
     # determine here if token is required
     ret_keys = ret.keys()
     if "count" in ret_keys:
         count = ret["count"]
     elif "code" in ret_keys:
         error_code = ret["code"]
-        print "error_code: {0}, message: {1}".format(error_code, ret["message"])
-
+        print "error_code: {0}\nmessage: {1}\n".format(error_code, ret["message"])
         user_id = get_out_db("select user_id from groups where group_id = {0} order by added desc".format(group_id))[0][0]
         auth_token = get_out_db("select auth_token from userinfo where user_id = {0}".format(user_id))
-        ret = getA(group_id, auth_token, 0, 1)
+        ret = getA(group_id, auth_token, 0, 2)
+        #print ret         
         if "count" in ret.keys():
             count = ret["count"]
         else:
             print "sorry... error_code: {0}, message: {1}".format(error_code, ret["message"])
+            save_log(ret)
             return group_id
     else:
         return group_id
-
+    print "--OK"
     save_into_db('delete from postinfo where group_id = {0}'.format(group_id))
     screen_name = get_out_db('select screen_name from groups where group_id = {0}'.format(group_id))[0][0]
     print "screen_name:", screen_name, ", group_id:", group_id, ", nums to parse:", str(count)
@@ -189,9 +200,13 @@ def full_cycle_v2(processed_groups, all_):
                 except:
                     repo = 0
                 try:
-                    picture = post['attachments'][0]['photo']['photo_130']
-                except:
                     picture = None
+                    if 'attachments' in post.keys():
+                        a_type = post['attachments'][0]['type']
+                        if a_type in ['photo', 'video']:
+                            picture = post['attachments'][0][a_type]['photo_130']
+                except Exception as ex:
+                    save_log("pic error: {0}".format(ex))
                 save_into_db("insert into postinfo (group_id, link, like, comm, repo, picture) values ({0}, '{1}', {2}, {3}, {4}, '{5}')".format(group_id, link, like, comm, repo, picture))
             except BaseException as ex:
                 save_log("link error: {0}".format(ex))
