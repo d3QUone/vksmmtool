17eb756182f9174bb19c4b4d60f386fd418915cc
Vladimir <volkvid@yandex.ru>
2015-01-23 14:59:17 +0300

    updated stats
    dat bug with db isn't fixed yet

--- vkparser.py
+++ vkparser.py
@@ -7,8 +7,8 @@ import sys
 import os
 
 
-# logging - OK
 def save_log(text):
+    # logging - OK
     text = "{0} | {1}\n".format(datetime.now(), text)
     try:
         f = open("logs.txt", "a")
@@ -21,8 +21,8 @@ def save_log(text):
     print text
 
 
-# request interface - OK
 def getA(group_id, auth_token=None, offset=0, count=100):
+    # request interface - OK
     if auth_token:
         url = "https://api.vk.com/method/execute.wall"
         data = {"i": "-{0}".format(group_id),
@@ -43,7 +43,7 @@ def getA(group_id, auth_token=None, offset=0, count=100):
         if "response" in rjson.keys():
             return rjson["response"]
         else:
-            print "\nfuck: {0}".format(rjson["error"])
+            print rjson["error"]
             return {"code": rjson["error"]["error_code"], "message": rjson["error"]["error_msg"]}
     except Exception as e:
         save_log("Requests error: {0}".format(e))
@@ -73,7 +73,8 @@ def save_into_db(statement):
         save_log("save_into_db error: {0}".format(e))
 
 
-def get_unique_groups(all_groups_raw):
+def get_unique_groups(sql_request):
+    all_groups_raw = get_out_db(sql_request)
     all_groups = []
     append = all_groups.append
     for item in all_groups_raw:
@@ -86,9 +87,12 @@ def controller():
     processed_groups = [] 
     append = processed_groups.append
     while True:
-        t = datetime.now()
-        all_groups_raw = get_out_db("select added, group_id from groups order by added desc")
-        all_groups = get_unique_groups(all_groups_raw)
+        all_groups = get_unique_groups("select added, group_id from groups order by added desc")
+        while len(all_groups) == 0:
+            print "len all = 0,", all_groups
+            time.sleep(3)
+            all_groups = get_unique_groups("select added, group_id from groups order by added desc")
+        
         try:
             new_id = full_cycle_v2(processed_groups, all_groups)
             if new_id:
@@ -99,28 +103,13 @@ def controller():
                 save_log("smth wrong, new_id={0}, len(processed_groups)={1}, len(all_groups)={2}".format(new_id, len(processed_groups), len(all_groups)))
                 time.sleep(3)
         except Exception as e:
-            save_log("full_cycle error: {0}, len(processed_groups)={1}, len(all_groups)={2}".format(e, len(processed_groups), len(all_groups)))
-            print "full_cycle error: {0}, len(processed_groups)={1}, len(all_groups)={2}".format(e, len(processed_groups), len(all_groups))
+            save_log("full_cycle: {0}, len(processed_groups)={1}, len(all_groups)={2}".format(e, len(processed_groups), len(all_groups)))
+            print "full_cycle: {0}, len(processed_groups)={1}, len(all_groups)={2}".format(e, len(processed_groups), len(all_groups))
             time.sleep(3)
             
         if len(all_groups) == len(processed_groups):
             print "\nall groups were updated!\n".upper()
             del processed_groups[:]
-            
-        # update stats
-        a = datetime.now()
-        a_h = a.hour
-        a_m = a.minute
-        if a_h < 10 and a_m < 10:
-            last_update = "0{0}:0{1}".format(a_h, a_m)
-        elif a_h >= 10 and a_m < 10:
-            last_update = "{0}:0{1}".format(a_h, a_m)
-        elif a_h < 10 and a_m >= 10:
-            last_update = "0{0}:{1}".format(a_h, a_m)
-        else:
-            last_update = "{0}:{1}".format(a_h, a_m)
-        dt = "{0}".format(a - t)
-        dt = dt.split(".")[0]
 
         # accurate write-in
         try:
@@ -129,10 +118,7 @@ def controller():
             f.close()
         except:
             data = {}
-        data["totalgroups"] = len(all_groups_raw)
-        data["totalposts"] = 0
-        data["time"] = dt
-        data["last_update"] = last_update
+        data["totalgroups"] = len(all_groups)
         
         stat = open(os.getcwd() + '/statistics.txt', 'w')
         stat.write(json.dumps(data))
@@ -145,7 +131,7 @@ def full_cycle_v2(processed_groups, all_):
     all_groups = all_
     i = 0
     chosen_id = -1
-    while i < len(all_groups):
+    while i < len(all_groups): # ---- check!
         item = all_groups[i]
         try:
             group_id = item[1]
@@ -154,19 +140,21 @@ def full_cycle_v2(processed_groups, all_):
                 if len(posts) == 0: # group is new or closed, anyway - we start with this group
                     chosen_id = group_id
                     break
-                else: # this group isnt new and isnt parsed
+                else: # this group isnt new and was parsed some time ago
                     i += 1
+                time.sleep(0.01)
             else: # this group is already parsed
                 all_groups.pop(i)
         except Exception as e:
             print "Allert: data error, {0}. Raw: {1}".format(e, item)
-    try:
-        # process chosen id           
+            
+    try:          
         if chosen_id == -1:
             group_id = all_groups[0][1] # -- first group of leftover
         else:
             group_id = chosen_id
     except Exception as e:
+        # bad all_groups
         print "choose_id exception: {0}, all_groups: {1}".format(e, all_groups)
         return None
     
@@ -178,16 +166,14 @@ def full_cycle_v2(processed_groups, all_):
     if "count" in ret_keys:
         count = ret["count"]
     elif "code" in ret_keys:
-        error_code = ret["code"]
-        print "error_code: {0}\nmessage: {1}\n".format(error_code, ret["message"])
+        print "error_code: {0}\nmessage: {1}\n".format(ret["code"], ret["message"])
         user_id = get_out_db("select user_id from groups where group_id = {0} order by added desc".format(group_id))[0][0]
-        auth_token = get_out_db("select auth_token from userinfo where user_id = {0}".format(user_id))
-        ret = getA(group_id, auth_token, 0, 1)
-        #print ret         
+        auth_token = get_out_db("select auth_token from userinfo where user_id = {0}".format(user_id))[0][0]
+        ret = getA(group_id, auth_token, 0, 1)        
         if "count" in ret.keys():
             count = int(ret["count"])
         else:
-            print "sorry... error_code: {0}, message: {1}".format(error_code, ret["message"])
+            print "sorry... error_code={0}, message: {1}".format(ret["code"], ret["message"])
             save_log(ret)
             return group_id
     else:
@@ -205,21 +191,20 @@ def full_cycle_v2(processed_groups, all_):
     except:
         data = {}
 
-    # load group-name...
     try:
         req = "https://api.vk.com/method/groups.getById?group_id={0}".format(group_id)
         written_name = requests.get(req).json()["response"][0]["name"]
     except Exception as ex:
         save_log("no written_name: {0}".format(ex))
         written_name = screen_name
-    print "written_name:", written_name
+    print "name:", written_name
 
     if len(written_name) > 32:
-        data["written_name"] = written_name[:32]
+        data["name"] = written_name[:32]
     else:
-        data["written_name"] = written_name
+        data["name"] = written_name
     data["count"] = count
-    data["link"] = "http://vk.com/" + screen_name
+    data["group_id"] = group_id
 
     stat = open(os.getcwd() + '/statistics.txt', 'w')
     stat.write(json.dumps(data))
@@ -252,14 +237,27 @@ def full_cycle_v2(processed_groups, all_):
                             picture = post['attachments'][0][a_type]['photo_130']
                 except Exception as ex:
                     picture = None
-                    #save_log("no key: {0}, link: {1}".format(ex, link))
                 save_into_db("insert into postinfo (group_id, link, like, comm, repo, picture) values ({0}, '{1}', {2}, {3}, {4}, '{5}')".format(group_id, link, like, comm, repo, picture))
             except BaseException as ex:
                 save_log("link error: {0}, full post: {1}, full response: {2}".format(ex, post, posts))
                 print "link error: ", ex, "full post: ", post
-        time.sleep(0.35)
+        # + update file here
+        f = open(os.getcwd() + '/statistics.txt', 'r')
+        data = json.loads(f.read())
+        f.close()
+        data["count"] -= len(posts["items"])
+        if data["count"] < 0:
+            data["count"] = 0
+        stat = open(os.getcwd() + '/statistics.txt', 'w')
+        stat.write(json.dumps(data))
+        stat.close()
+        
+        time.sleep(0.1)
     return group_id
 
-
+ver = "2a"
 if __name__ == "__main__":
+    save_log("vkparser v={0} is running!".format(ver))
+    print "vkparser v={0} is running!".format(ver)
+    print "--"*25
     controller()
