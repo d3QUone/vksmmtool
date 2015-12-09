10b80470e86cd6af1d441a92b029ccbee19ba180
Vladimir <volkvid@yandex.ru>
2015-12-09 19:20:24 +0300

    covered wrapping, lol :)

--- tests.py
+++ tests.py
@@ -4,8 +4,16 @@ import pytest
 
 from server import wrap_value, unwrap_value
 
+
 def test_wrapping():
     val1 = "'test"
     val2 = wrap_value(val1)
-    val3 = "&#39;"
+    val3 = "&#39;test"
+    assert val2 == val3
+
+
+def test_unwrapping():
+    val1 = "&#39;test"
+    val2 = unwrap_value(val1)
+    val3 = "'test"
     assert val2 == val3
